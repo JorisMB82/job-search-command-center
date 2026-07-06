63689f57e59d39ac73f07ea0f22adea9b254b891
JorisMB82 <joris@jvlimited.com>
2026-07-06 17:03:49 -0400

    Delete pipeline/page.tsx

--- pipeline/page.tsx
+++
@@ -1,9 +0,0 @@
-import { PipelineClient } from "../../components/PipelineClient";
-import { requireAuth } from "../../lib/auth";
-
-export const dynamic = "force-dynamic";
-
-export default async function PipelinePage() {
-  await requireAuth();
-  return <PipelineClient />;
-}
